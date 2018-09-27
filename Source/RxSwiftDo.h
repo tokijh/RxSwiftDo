c2e73b65d2c44958e4d5cb8bd646d7ad2a612508
tokijh <tokijh@naver.com>
2018-09-27 11:52:21 +0900

    Setup RxSwiftDo project for iOS

--- Source/RxSwiftDo.h
+++ Source/RxSwiftDo.h
@@ -6,7 +6,7 @@
 //  Copyright © 2018년 윤중현. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for RxSwiftDo.
 FOUNDATION_EXPORT double RxSwiftDoVersionNumber;
